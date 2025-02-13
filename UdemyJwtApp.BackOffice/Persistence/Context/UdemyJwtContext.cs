7ef361d5d7da4e87842ac0c283d5572e695381e6
Furkan <furkankucukali@gmail.com>
2025-02-13 12:05:16 +0300

    'gitignore'

---
+++ UdemyJwtApp.BackOffice/Persistence/Context/UdemyJwtContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UdemyJwtApp.BackOffice.Core.Domain;
+using UdemyJwtApp.BackOffice.Persistence.Configurations;
+
+namespace UdemyJwtApp.BackOffice.Persistence.Context
+{
+    public class UdemyJwtContext : DbContext
+    {
+        public UdemyJwtContext(DbContextOptions<UdemyJwtContext> options): base(options) { 
+        
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new AppUserConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
