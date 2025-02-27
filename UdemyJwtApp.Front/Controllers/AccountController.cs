2654c501da16a93ce3d2de9d41158f87d1f01329
Furkan <furkankucukali@gmail.com>
2025-02-27 14:14:05 +0300

    add sign in page and token base system

--- UdemyJwtApp.Front/Controllers/AccountController.cs
+++ UdemyJwtApp.Front/Controllers/AccountController.cs
@@ -1,19 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
 using UdemyJwtApp.Front.Models;
 
 namespace UdemyJwtApp.Front.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public AccountController(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Login()
         {
             return View(new UserLoginModel());
         }
         [HttpPost]
-        public IActionResult Login(UserLoginModel model) {
+        public async Task<IActionResult> Login(UserLoginModel model) {
             if (ModelState.IsValid)
             {
-                return View();
+                var client = this.httpClientFactory.CreateClient();
+
+                var content = new StringContent(JsonSerializer.Serialize(model) , Encoding.UTF8, "application/json");
+                var response = await client.PostAsync("http://localhost:5008/api/Auth/Login", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                   var tokenModel= JsonSerializer.Deserialize<JwtTokenResponseModel>(jsonData ,new JsonSerializerOptions
+                   {
+                       PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                   });
+
+                    if (tokenModel != null) {
+                        JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                        var token = handler.ReadJwtToken(tokenModel.Token);
+                        var claimsIdentity = new ClaimsIdentity(token.Claims,JwtBearerDefaults.AuthenticationScheme);
+
+                        var authProps = new AuthenticationProperties
+                        {
+                            ExpiresUtc = tokenModel.ExpireDate,
+                            IsPersistent = true
+                        };
+
+                        await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity),authProps);
+
+                        return RedirectToAction("Index","Home");
+
+                    }
+                  
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Kullanıcı adı veya şifre Hatalı");
+
+                }
+                return View(model);
+
             }
             return View(model);
         }
