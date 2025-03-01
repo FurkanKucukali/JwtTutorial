7ff0f883d9da5c9511e2e98bb8dcb458e51e6350
Furkan <furkankucukali@gmail.com>
2025-03-01 23:56:53 +0300

    create category feature in front

--- UdemyJwtApp.Front/Controllers/CategoryController.cs
+++ UdemyJwtApp.Front/Controllers/CategoryController.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Text;
 using System.Text.Json;
 using UdemyJwtApp.Front.Models;
 
 namespace UdemyJwtApp.Front.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     public class CategoryController : Controller
     {
         private readonly IHttpClientFactory httpClientFactory;
@@ -17,16 +18,18 @@ namespace UdemyJwtApp.Front.Controllers
 
         public async Task<IActionResult> List()
         {
-            var token = User.Claims.FirstOrDefault(x=> x.Type == "accesToken")?.Value;
-            if (token != null) { 
-            var client = this.httpClientFactory.CreateClient();
+            var token = User.Claims.FirstOrDefault(x => x.Type == "accesToken")?.Value;
+            if (token != null)
+            {
+                var client = this.httpClientFactory.CreateClient();
 
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                var response =  await client.GetAsync("https://localhost:7134/api/categories");
-                if (response.IsSuccessStatusCode) {
-                
-                   var jsonData =  await response.Content.ReadAsStringAsync();
-                   var result = JsonSerializer.Deserialize<List<CategoryListModel>>(jsonData, new JsonSerializerOptions
+                var response = await client.GetAsync("https://localhost:7134/api/categories");
+                if (response.IsSuccessStatusCode)
+                {
+
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    var result = JsonSerializer.Deserialize<List<CategoryListModel>>(jsonData, new JsonSerializerOptions
                     {
                         PropertyNamingPolicy = JsonNamingPolicy.CamelCase
                     });
@@ -45,12 +48,43 @@ namespace UdemyJwtApp.Front.Controllers
                 var client = this.httpClientFactory.CreateClient();
 
                 client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-                var response = await client.DeleteAsync($"https://localhost:7134/api/categories/{id}");               
-                
+                var response = await client.DeleteAsync($"https://localhost:7134/api/categories/{id}");
+
             }
             return RedirectToAction("List");
 
+        }
+        public IActionResult Create()
+        {
+            return View(new CreateCategoryModel());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var token = User.Claims.FirstOrDefault(x => x.Type == "accesToken")?.Value;
+                if (token != null)
+                {
+                    var client = this.httpClientFactory.CreateClient();
+                    var jsonData = JsonSerializer.Serialize(model);
+                    var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
 
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+                    var response = await client.PostAsync("https://localhost:7134/api/categories", content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return RedirectToAction("List");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Bir Hata Oluştu");
+                    }
+
+                }
+            }
+            return View(model);
 
         }
     }
